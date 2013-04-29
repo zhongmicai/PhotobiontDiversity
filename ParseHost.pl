8b170f44b8a2679a7d40ff4c851d65e185a25c4f
hobrien <heath.obrien@gmail.com>
2013-04-29 16:14:15 +0100

    See http://photobiontdiversity.wordpress.com/2013/04/29/adding-host-information

--- ParseHost.pl
+++ ParseHost.pl
@@ -3,5 +3,6 @@ use warnings;
 use strict;
 while (<>){
   chomp;
-  if ( $_ =~ /gi\|\d+\|\w+\|(\w+)\.\d+\|[^']*'(\w+\s+[\w.]+)[()\d\s]*cyanobiont'/ ) { print "$1\t$2\n"; }
+  if ( $_ =~ /gi\|\d+\|\w+\|(\w+)\.\d+\|[^']*'(\w+\s+[\w.() ]+)cyanobiont'/ ) { print "$1\t$2\n"; }
+  elsif ( $_ =~ /^>/ ) { print STDERR "$_: could not parse host info\n"; }
 }
