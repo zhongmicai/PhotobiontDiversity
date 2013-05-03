5b3bc6d9bac7a2c50766914d93590c93bc36bb83
hobrien <heath.obrien@gmail.com>
2013-05-03 17:33:21 +0100

    Instructions for what colouros to assign to each host genus

---
+++ Scripts/PhotobiontCategories.pm
@@ -0,0 +1,70 @@
+%lichen_colors = ('Collema', '#ED99AA',
+            'Degelia', '#D5A971',
+            'Leptogium', '#9DBB6A',
+            'Lobaria', '#4BC49E',
+            'Massalongia', '#45C0D4',
+            'Nephroma', '#A7ACEC',
+            'Pannaria', '#E298D6',
+            'Parmeliella', '#890939',
+            'Peltigera', '#6A3B00',
+            'Protopannaria', '#285000',
+            'Pseudocyphellaria', '#005C26',
+            'Stereocaulon', '#005D77',
+            'Sticta', '#28399F'
+            );
+
+%taxonomy = ('Collema', 'Collemataceae',
+            'Degelia', 'Pannariaceae',
+            'Leptogium', 'Collemataceae',
+            'Lobaria', 'Lobariaceae',
+            'Massalongia', 'unknown',
+            'Nephroma', 'Nephromataceae',
+            'Pannaria', 'Pannariaceae',
+            'Parmeliella', 'Pannariaceae',
+            'Peltigera', 'Peltigeraceae',
+            'Protopannaria', 'Pannariaceae',
+            'Pseudocyphellaria', 'Lobariaceae',
+            'Stereocaulon', 'Stereocaulaceae',
+            'Sticta', 'Lobariaceae'
+            );
+
+%host = ('Collema', 'Lichen',
+            'Degelia', 'Lichen',
+            'Leptogium', 'Lichen',
+            'Lobaria', 'Lichen',
+            'Massalongia', 'Lichen',
+            'Nephroma', 'Lichen',
+            'Pannaria', 'Lichen',
+            'Parmeliella', 'Lichen',
+            'Peltigera', 'Lichen',
+            'Protopannaria', 'Lichen',
+            'Pseudocyphellaria', 'Lichen',
+            'Stereocaulon', 'Lichen',
+            'Sticta', 'Lichen',
+            'Anthoceros', 'Plant',
+            'Blasia', 'Plant',
+            'Cylindrospermum', 'Free-living',
+            'Encephalartos', 'Plant',
+            'Geosiphon', 'Other',
+            'Macrozamia', 'Plant',
+            'Nostoc', 'Free-living
+         );
+            
+
+%host_colors = ('Plant', '#317B46',
+                'Free-living', '#008A79',
+                'Lichen', '#27575C',
+                'Other', '#522671'
+                );
+                
+
+%family_colors = ('Collemataceae', '#C87A8A',
+                  'Pannariaceae', '#AE8B50',
+                  'Lobariaceae', '#729C55',
+                  'unknown', '#000000,
+                  'Nephromataceae', '#00A38F',
+                  'Peltigeraceae', '#4C99BE',
+                  'Stereocaulaceae', '#A782C3'
+            );
+
+#If not in set, use #A0A0A0
