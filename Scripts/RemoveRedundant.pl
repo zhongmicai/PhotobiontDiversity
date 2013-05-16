e78f8e90b31826f111c861b6e1f62e7a5a23ce34
hobrien <heath.obrien@gmail.com>
2013-05-16 10:24:09 +0100

    Modified to exclude taxa listed as redundant rather than to include ones listed as IN TREE
    This means that it includes uncategorized sequences

--- Scripts/RemoveRedundant.pl
+++ Scripts/RemoveRedundant.pl
@@ -6,13 +6,13 @@ use Bio::SeqIO;
 
 my $infilename = shift;
 
-my %included;
+my %redundant;
 while (<>) {
   chomp;
   if ( $_ =~ /^\s*$/ ) { next; }  #skip blank lines
   $_ =~ s/,\s+$//;   #remove trailing comma and whitespace
   my @fields = split(/\s*\t\s*/, $_);
-  if ( $fields[2] =~ /IN TREE/ ) { $included{$fields[0]} = 1; }
+  if ( $fields[2] =~ /REDUNDANT/ ) { $redundant{$fields[0]} = 1; }
 }
 
 my $infile = Bio::SeqIO->new('-file' => $infilename,
@@ -22,5 +22,5 @@ my $outfile = Bio::SeqIO->new('-file' => "| cat",
          '-format' => 'fasta') or die "could not open seq file $infilename\n";
 
 while ( my $seq = $infile->next_seq ) {
-  if ( $included{$seq->display_id} ) { $outfile->write_seq($seq); }
+  unless ( $redundant{$seq->display_id} ) { $outfile->write_seq($seq); }
 }
