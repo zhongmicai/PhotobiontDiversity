4bfacbf4aac7f20feaef9a8b1a98b9702c58f9dc
hobrien <heath.obrien@gmail.com>
2013-08-16 16:17:55 +0100

    Added documentation

--- Scripts/AddDuplicates.pl
+++ Scripts/AddDuplicates.pl
@@ -1,4 +1,32 @@
-use warnings;
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+AddDuplicates.pl version 1, 5 August 2013
+
+=head1 SYNOPSIS
+
+cat duplicates.txt | AddDuplicates.pl matadata_file.txt >metadata_file2.txt
+
+=head1 DESCRIPTION
+
+This script will add duplicate entries for genbank sequences that represent multiple specimens with the same haplotype
+
+duplicates.txt should be a tab delimited file with accession number in 1st column, host in
+the 2nd and number of specimens represented in the third.
+
+metadata_file should be a tab delimited file with accession number in 1st column and host
+should be the second. All other columns are duplicated.
+
+Accession numbers are appended with a period and a sequential three digit number 
+=head2 NOTES
+
+=head1 AUTHOR
+
+ Heath E. O'Brien E<lt>heath.obrien-at-gmail-dot-comE<gt>
+
+=cut
+####################################################################################################use warnings;
 use strict;
 
 my $metadatafile = shift;
