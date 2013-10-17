0cdc323d184baaa54decce7818c5f7dd1521a18e
hobrien <heath.obrien@gmail.com>
2013-10-17 17:09:25 +0100

    Major refactoring to save info to MySQL db rather than to text file
    Group names in db are overwritten if present
    
    I am no longer worrying about which sequences are in the non-redundant dataset because
    I am looping through taxa in the tree and looking up the info in the db rather than looping
    through the info in the metadatafile and doing replacements to the tree file

--- Scripts/GetGroups.py
+++ Scripts/GetGroups.py
@@ -1,62 +1,72 @@
 #!/usr/local/bin/python
-#assign new sequences to pre-defined Trebouxia clades. Info is added to the 7th column of the metadata file
-#this would be more robust if it used all of the sequences that have been assigned to clades to define the clades.
+
+"""Replaces 'Group' info in the database for the given gene with info from the specified
+group file (output from usearch).
+
+There are a bunch of complex functions included here that are no longer necessary, but
+I don't have the heart to remove them because they aren't backed up anywhere
+
+"""
+
 import sys, getopt, string
-from ete2 import Tree
 import csv
+import MySQLdb as mdb
 
 def main(argv):
   groupfile = ''
-  metadatafile = ''
+  gene = ''
+  usage = 'GetGroups.py -g groupfile> -l <locus>'
   try:
-    opts, args = getopt.getopt(argv,"hm:g:",["tree=","metadata="])
+    opts, args = getopt.getopt(argv,"hl:g:",["group=","locus="])
   except getopt.GetoptError:
-    print 'Type GetClades.py -h for options'
+    print usage
     sys.exit(2)
   for opt, arg in opts:
     if opt == "-h":
-       print 'GetClades.py -g <groupfile> -m <metadata_file>'
+       print usage
        sys.exit()
     elif opt in ("-g", "--group"):
        groupfile = arg
-    elif opt in ("-m", "--metadata"):
-       metadatafile = arg
+    elif opt in ("-l", "--locus"):
+       gene = arg
 
-  groups, representatives = GetGroups(groupfile)
+  groups = GetGroups(groupfile)
+  con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+  with con:
+    cur = con.cursor()
+    #remove saved group info from db
+    cur.execute("UPDATE Metadata SET `Group` = NULL WHERE Gene= %s", (gene,))     
+    for group in groups.keys():
+      for accession in groups[group]:
+        cur.execute("UPDATE Metadata SET `Group` = %s WHERE Accession= %s AND Gene= %s", (group, accession, gene,))
 
-  with open(metadatafile, 'rU') as f:
-    reader=csv.reader(f,delimiter='\t')
-    for row in reader:
-      clade = ""
-      if len(row) == 11:
-        accession, group, tree_status, host, name, clade, strain, location, author, reference, pmid = row
-      elif len(row) == 10:      
-        accession, group, tree_status, host, name, strain, location, author, reference, pmid = row
-      elif len(row) == 9:
-        accession, host, name, clade, strain, location, author, reference, pmid = row
-      elif len(row) == 8:
-        accession, host, name, strain, location, author, reference, pmid = row
-      else: raise ValueError("Metadata file does not have the right number of columns. Format should be 'accession, [group, tree_status,] host, name, [clade,] strain, location, author, reference, pmid'\n")
-      
-      #Add group number info
-      base = string.split(accession, ".")[0]  #strip off part after decimal indicating multiple samples with same haplotype
-      if base in representatives.keys(): group = "Group " + format(representatives[base], "03d")
-      elif base in groups.keys(): group = "Group " + format(groups[base], "03d")
-      else: group = 'NA'
-      
-      tree_status = "REDUNDANT"
-      if base in representatives.keys():
-        if accession == base:
-          tree_status = "IN TREE"  
-          if representatives[accession] not in groups.values():
-            group = "UNIQUE"
-        elif accession == base + ".000":
-          tree_status = "IN TREE"  
-      print "\t".join([accession, group, tree_status, host, name, clade, strain, location, author, reference, pmid])
-   
+         
+def update_saved(group_list, gene):
+  con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+  with con:
+    cur = con.cursor()
+    for group in group_list[1:]:
+      print "setting %s to %s:" % (group, group_list[0])
+      cur.execute("SELECT Accession FROM Metadata WHERE `Group`= %s AND Gene= %s", (group,gene,))
+      for row in cur.fetchall():
+        cur.execute("UPDATE Metadata SET `Group` = %s WHERE Accession= %s AND Gene= %s", (group_list[0], row[0], gene,))
+
+  
+def unique_group(gene):
+  con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+  with con:
+    cur = con.cursor()
+    for group_num in range(10000):
+      group = "Group " + format(group_num, "03d")
+      cur.execute("SELECT * FROM Metadata WHERE `Group`= %s AND Gene= %s", (group,gene,))
+      if not cur.fetchone():
+        break
+  return group
+        
 def GetGroups(file):
+  """Parses usearch output and assignes each non-singleton sequence to a group.
+  """
   groups = {}             #only groups with multiple seqs
-  representatives = {}    #only seqs that are included in nr set
   with open(file, 'rU') as f:
     reader=csv.reader(f,delimiter='\t')
     for type, group, length, percent_id, strand, x1, x2, aln, query, hit in reader:
@@ -66,10 +76,35 @@ def GetGroups(file):
         accession = string.split(query,".")[0]
       else:
         accession = query
-      if type == 'S': representatives[accession] = int(group)
-      elif type == 'H': groups[accession] = int(group)
-  return groups, representatives
+      if type == 'S' or type == 'H':
+        group_name = "Group " + format(int(group), "03d")
+        if group_name in groups.keys():
+          groups[group_name] += [accession]
+        else:
+          groups[group_name] = [accession]
   
+  return groups
+  
+def retrieve_groups(group, gene):
+  """Looks up each sequence from each group in database and if it is assigned to a group 
+  in the db, all sequences assigned to the same group are given that group number"""
+  
+  saved_groups = []
+  con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+  with con:
+    cur = con.cursor()
+    for accession in group:   #Assigned saved group numbers from db to groups with representatives in db
+      cur.execute("SELECT `Group` FROM Metadata WHERE Accession= %s AND Gene= %s", (accession, gene,))
+      db_group = cur.fetchone()[0]
+      if db_group and db_group != 'UNIQUE':            #accession is already assigned to a group in the database
+        if db_group not in saved_groups:
+            saved_groups += [db_group]
+          
+  return saved_groups
+    
+def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
+    import warnings
+    return ' %s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
