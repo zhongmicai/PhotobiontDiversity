288fe3ed7f6fa48a6f2bc73ea9dc3c37f5b38bb5
hobrien <heath.obrien@gmail.com>
2013-10-18 16:32:15 +0100

    Added functionality to draw tree with host / species names and colour-coding
    Script now uses the tree drawing functionality of ETE2 to draw a tree putting each host
    name in a separate face, colour coded according to the info in the database (phylum name
    except for the Ascomycota, where family is used)
    
    If more than 3 host names are needed, multiple rows are used
    
    I'm not wild about how the root of the tree is drawn, but I'm going with it for now.

--- Scripts/AddMetadata.py
+++ Scripts/AddMetadata.py
@@ -10,13 +10,20 @@ which sequences are actually in the tree. This means I need to loop through the 
 the tree rather than the "IN TREE" entries in the database. This should be straightforward
 with ETE
 
-Not least of which is that I'm going to switch from perl to python
+This is working great now, but I'd like to add the tree drawing into this script
+because I can use text faces rather than taxon names, which will greatly increase flexibility
 
+That's now working reasonably well, though I don't like how the root is drawn
+
+Next I need to add colours based on taxonomy
+
+I also have to fix the issue where, eg; Pannaria and Pseudocyphellaria are both abbreviated P.
+I should also sort the host names to make things clearer
 """
 
 import sys, getopt, string, warnings
 import MySQLdb as mdb
-from ete2 import Tree  
+from ete2 import Tree, TreeStyle, TextFace, NodeStyle  
 
 def main(argv):
   treefilename = ''
@@ -44,10 +51,10 @@ def main(argv):
   tree = Tree(treefilename)
 
   con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
-  with con:
-    cur = con.cursor()
-    groups = {}
-    for leaf in tree:
+  for leaf in tree:
+    with con:
+      cur = con.cursor()
+      groups = {}
       cur.execute("SELECT `Group`, Host, Species FROM Metadata WHERE Accession= %s AND Gene= %s", (leaf.name, locus,))
       try:
         (group, host, species) = cur.fetchone()
@@ -60,15 +67,84 @@ def main(argv):
         else:
           groups[group] = leaf.name
           cur.execute("SELECT Host, Species FROM Metadata WHERE `Group`= %s AND Gene= %s", (group, locus,))
-          leaf.name = ' '.join([group, combine_info(cur.fetchall())])
+          leaf.name = " " + group + ':'
+          label_info = combine_info(cur.fetchall())
       else:  #Singleton
+        leaf.name =" " + leaf.name + ':'
         if host and host != 'free-living':
-          leaf.name = ' '.join([leaf.name, host])
+          label_info = [host]
         else:    
-          leaf.name = ' '.join([leaf.name, species])
+          label_info = [species]
 
-    tree.write(format=0, outfile=outfilename)
+    add_faces(leaf, label_info)
+   
+  draw_tree(tree, outfilename)   
+
+def draw_tree(tree, file):
+    root = tree.get_tree_root()
+    root.dist = 0
+    add_sig(tree)
+    ts = TreeStyle()
+    ts.branch_vertical_margin = 1
+    ts.scale = 500
+    tree.render(file, tree_style=ts, w=3000, units='mm')
+
+def add_faces(leaf, label_info):
+      colours = get_colours(label_info)
+      y = 0
+      for x in range(len(label_info)):
+        if x < len(label_info) - 1:
+          label_info[x] += ','
+        label = TextFace(label_info[x])
+        label.margin_left = 5
+        label.fgcolor = colours[x]
+        if x > 1 and x % 3 == 0:
+          y += 3
+        leaf.add_face(label, column=x-y+1, position="branch-right")
       
+def get_colours(label_info):
+  colours = []
+  for label in label_info:
+    genus = label.split(' ')[0]
+    if genus.find('.') != -1:
+      colours.append(colours[-1])
+    else:
+      con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+      with con:
+        cur = con.cursor()
+        try:
+          cur.execute("SELECT phylum FROM Taxonomy WHERE genus= %s", (genus))
+          taxon = cur.fetchone()
+        except TypeError:
+          warnings.warn("No phylum entry for %s" % genus)
+        if taxon and taxon[0] == 'Ascomycota':
+          try:
+            cur.execute("SELECT family FROM Taxonomy WHERE genus= %s", (genus))
+            taxon = cur.fetchone()
+          except TypeError:
+            warnings.warn("No family entry for %s" % genus)
+        try:
+          cur.execute("SELECT Colour FROM Colours WHERE Taxon= %s", (taxon[0]))
+          colour = cur.fetchone()      
+          colours.append(colour[0])
+        except TypeError:
+          warnings.warn("No colour available for %s (%s)" % (genus, taxon))
+          colours.append('LightGray')
+          
+  return colours
+    
+def add_sig(tree):
+  non_sig = NodeStyle()
+  non_sig["size"] = 0
+  sig = NodeStyle()
+  sig["size"] = 5
+  sig["fgcolor"] = "black"
+  for node in tree.traverse():
+    if node.support < 0.9 or node.is_leaf():
+      node.set_style(non_sig)
+    else:
+      node.set_style(sig)
+
 def combine_info(entries):
   host_counts = {}                   #Can include species names of free-living strains
   for (host, species) in entries:
@@ -80,20 +156,23 @@ def combine_info(entries):
       host_counts[info] += 1
     else:
       host_counts[info] = 1
-  out_string = ''
+  out_list = []
   included_genera = []
-  for name in host_counts.keys():
+  names = host_counts.keys()
+  names.sort()
+  for name in names:
       count = host_counts[name]
       genus = name.split(' ')[0]
       if genus in included_genera:
         name = name.replace(genus,  genus[0] + '.')
       else:
         included_genera.append(genus)
-      if out_string:
-        out_string += ", %s (%s)" % ( name, count)
+      if count > 1:  
+        out_list.append("%s (%s)" % ( name, count))
       else:
-         out_string = "%s (%s)" % ( name, count)
-  return out_string
+        out_list.append(name)
+
+  return out_list
   
 if __name__ == "__main__":
    main(sys.argv[1:])
