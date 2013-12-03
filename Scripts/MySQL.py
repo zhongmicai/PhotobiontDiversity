82ac559074cc4d4f7df9fa2f135f629cf07410db
hobrien <heath.obrien@gmail.com>
2013-12-03 10:52:14 +0000

    first commit

---
+++ Scripts/MySQL.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+
+import sys, fileinput
+import MySQLdb as mdb
+
+try:
+    con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
+    cur = con.cursor()
+    for line in fileinput.input():
+        accession = line.strip()
+        cur.execute("SELECT * FROM Metadata WHERE Accession= %s", (accession,))
+        if not cur.fetchone():
+            print accession
+except mdb.Error, e:
+  
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    sys.exit(1)
+    
+finally:    
+        
+    if con:    
+        con.close()
+        
