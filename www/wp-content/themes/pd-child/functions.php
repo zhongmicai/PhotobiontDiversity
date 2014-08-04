6a821296eb44c1f9a50ca6d24ad2b5493069211d
hobrien <heath.obrien@gmail.com>
2014-08-04 17:12:01 +0100

    remove website files from repo

--- www/wp-content/themes/pd-child/functions.php
+++
@@ -1,7 +0,0 @@
-add_action( 'init', 'mytheme_setup' );
-add_theme_support( 'post-thumbnails' );
- 
-function mytheme_setup() {
-set_post_thumbnail_size( 100, 100, true );
-add_image_size( 'single-post-thumbnail', 250, 250 );
-}
