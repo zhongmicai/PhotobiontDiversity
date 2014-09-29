b5cc7d90b9ee1c418e6f8756ccde4a3dbe14f06e
hobrien <heath.obrien@gmail.com>
2014-09-29 14:30:54 +0100

    added additional fields, error catching

--- Scripts/GetTaxonomy.py
+++ Scripts/GetTaxonomy.py
@@ -1,3 +1,4 @@
+#!/Users/HeathOBrien/anaconda/bin/python
 import sys, warnings
 from Bio import Entrez
 import MySQLdb as mdb
@@ -21,15 +22,16 @@ def get_tax_data(taxid):
     search = Entrez.efetch(id = taxid, db = "taxonomy", retmode = "xml")
     return Entrez.read(search)
 
-Entrez.email = sys.argv[1]
-if not Entrez.email:
+try:
+  Entrez.email = sys.argv[1]
+except IndexError:
     print "you must add your email address"
     sys.exit(2)
 
 con = mdb.connect('localhost', 'root', '', 'PhotobiontDiversity');
 with con:
     cur = con.cursor()
-    cur.execute("SELECT Host, Species FROM Metadata")
+    cur.execute("SELECT Host, Species FROM Metadata", )
     genera = []
     for names in cur.fetchall():
       for name in names:
@@ -41,33 +43,58 @@ with con:
             genera.append(genus)
 
     for genus in genera:
-      taxid = get_tax_id(genus)
-      if taxid:
-        cur.execute("INSERT INTO Taxonomy(id, genus) VALUES(%s, %s)", (taxid, genus,))
-        data = get_tax_data(taxid)
-        lineage = {d['Rank']:d['ScientificName'] for d in 
-          data[0]['LineageEx']}
-        for rank in lineage.keys():
-          if rank == 'family':
-            cur.execute("UPDATE Taxonomy SET family = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'order':
-            cur.execute("UPDATE Taxonomy SET `order` = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'subclass':
-            cur.execute("UPDATE Taxonomy SET subclass = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'class':
-            cur.execute("UPDATE Taxonomy SET class = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'subphylum':
-            cur.execute("UPDATE Taxonomy SET subphylum = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'phylum':
-            cur.execute("UPDATE Taxonomy SET phylum = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'subkingdom':
-            cur.execute("UPDATE Taxonomy SET subkingdom = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'kingdom':
-            cur.execute("UPDATE Taxonomy SET kingdom = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'superkingdom':
-            cur.execute("UPDATE Taxonomy SET superkingdom = %s WHERE genus= %s", (lineage[rank], genus,))
-          elif rank == 'no rank':
-            cur.execute("UPDATE Taxonomy SET no_rank = %s WHERE genus= %s", (lineage[rank], genus,))
-          else:
-            warnings.warn("No %s field in database" % rank)
-
+      cur.execute("SELECT * FROM Taxonomy WHERE Genus = %s", genus)
+      if len(cur.fetchall()) == 0:
+        taxid = get_tax_id(genus)
+        if taxid:
+          try:
+            cur.execute("INSERT INTO Taxonomy(id, genus) VALUES(%s, %s)", (taxid, genus,))
+          except mdb.IntegrityError:
+            sys.exit("duplicate entry for %s taxid (%s)" % (genus, taxid))
+          data = get_tax_data(taxid)
+          lineage = {d['Rank']:d['ScientificName'] for d in 
+            data[0]['LineageEx']}
+          for rank in lineage.keys():
+            if rank == 'family':
+              cur.execute("UPDATE Taxonomy SET family = %s WHERE genus= %s", (lineage[rank], genus,))
+              #cur.execute("SELECT Colour FROM Colours WHERE Taxon = %s", lineage[rank])
+              #if len(cur.fetchall()) == 0:
+              #  print "No colour in DB for %s" % lineage[rank]
+            elif rank == 'tribe':
+              cur.execute("UPDATE Taxonomy SET tribe = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'subfamily':
+              cur.execute("UPDATE Taxonomy SET subfamily = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'superfamily':
+              cur.execute("UPDATE Taxonomy SET superfamily = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'order':
+              cur.execute("UPDATE Taxonomy SET `order` = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'suborder':
+              cur.execute("UPDATE Taxonomy SET `suborder` = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'infraorder':
+              cur.execute("UPDATE Taxonomy SET `infraorder` = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'subclass':
+              cur.execute("UPDATE Taxonomy SET subclass = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'infraclass':
+              cur.execute("UPDATE Taxonomy SET infraclass = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'class':
+              cur.execute("UPDATE Taxonomy SET class = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'superclass':
+              cur.execute("UPDATE Taxonomy SET superclass = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'subphylum':
+              cur.execute("UPDATE Taxonomy SET subphylum = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'phylum':
+              cur.execute("UPDATE Taxonomy SET phylum = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'superphylum':
+              cur.execute("UPDATE Taxonomy SET superphylum = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'subkingdom':
+              cur.execute("UPDATE Taxonomy SET subkingdom = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'kingdom':
+              cur.execute("UPDATE Taxonomy SET kingdom = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'superkingdom':
+              cur.execute("UPDATE Taxonomy SET superkingdom = %s WHERE genus= %s", (lineage[rank], genus,))
+            elif rank == 'no rank':
+              cur.execute("UPDATE Taxonomy SET no_rank = %s WHERE genus= %s", (lineage[rank], genus,))
+            else:
+              warnings.warn("No %s field in database" % rank)
+      #else:
+        #print "%s already in taxonomyDB" % genus
