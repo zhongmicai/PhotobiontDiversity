746d8671f416f2890a570f330477d45801b19b11
Heath O'Brien <heath.obrien@gmail.com>
2016-09-05 15:34:49 +0200

    add database path info

--- Scripts/PhotobiontDiversity.sh
+++ Scripts/PhotobiontDiversity.sh
@@ -4,5 +4,6 @@
 #$ -j y
 #$ -S /bin/bash
 #
+export BLASTDB="/c8000xd3/rnaseq-heath/DB"
 cd /c8000xd3/rnaseq-heath/PhotobiontDiversity/160905
-blastn -query ../ReferenceSeqs/Trebouxia_ITS.fa -db /c8000xd3/rnaseq-heath/DB/nt -evalue 1e-180 -max_target_seqs 10000 -out Trebouxia_ITs.bl -outfmt '6 qseqid qlen sacc slen pident length mismatch gapopen qstart qend qframe sstart send sframe evalue bitscore'
+blastn -query ../ReferenceSeqs/Trebouxia_ITS.fa -db nt -evalue 1e-180 -max_target_seqs 10000 -out Trebouxia_ITs.bl -outfmt '6 qseqid qlen sacc slen pident length mismatch gapopen qstart qend qframe sstart send sframe evalue bitscore'
