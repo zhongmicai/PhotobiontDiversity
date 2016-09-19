d22b757bc1254620d33d20abc03ad5484a44a96c
Heath O'Brien <heath.obrien@gmail.com>
2016-09-18 22:04:32 -0600

    add some message printing

--- Scripts/PhotobiontDiversity.sh
+++ Scripts/PhotobiontDiversity.sh
@@ -10,7 +10,9 @@ export PERL5LIB=$HOME/src/bioperl-live:$HOME/Bio-Root:$HOME/perl5/lib/perl5
 
 cd /c8000xd3/rnaseq-heath/PhotobiontDiversity/160905
 dataset=TrebouxiaITS
+echo "blasting $dataset"
 blastn -query ../ReferenceSeqs/Trebouxia_ITS.fa -db nt -evalue 1e-180 -max_target_seqs 10000 -out $dataset.bl -outfmt '6 qseqid qlen sacc slen pident length mismatch gapopen qstart qend qframe sstart send sframe evalue bitscore'
+echo "finished blasting $dataset"
 awk '{if($12<$13) print $0}' ${dataset}.bl | awk '{if ($4 < 3000) print $0}' |cut -f3 | sort | uniq > ${dataset}_acc.txt
 
 #echo "`date`: blastdbcmd -db nt -entry_batch ${dataset}_acc.txt |../Scripts/GetRedundant.pl >${dataset}_all.fa" >> $log_file
